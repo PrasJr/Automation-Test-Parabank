99f21a1903db7e2333040abe5998a11225aa7359
PrasJr <budiprasetyo32@gmail.com>
2023-03-23 14:33:50 +0700

    typo

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,7 +10,7 @@ options.add_argument('--headless') if ENV['HEADLESS']
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
 
-Capybara.register_driver :chrome do |app|
+Capybara.register_driver :selenium_chrome do |app|
     Capybara::Selenium::Driver.new(app, browser: :chrome, timeout: 30, options: options)
 end
 
